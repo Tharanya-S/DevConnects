b5e687b37197abc0294704c62fde301ed3bd1066
Tharanya <tharanyasugumaran@gamil.com>
2024-10-19 12:10:43 +0530

    exploring GET , POST HTTP request

--- src/app.js
+++ src/app.js
@@ -10,6 +10,23 @@ app.use("/hello", (req, res) => {
   res.send("Hello World !!!");
 });
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Tharanya", lastName: "Sugumaran" });
+});
+
+app.post("/user", (req, res) => {
+  //Logic to add data
+  res.send("Users added successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Patched user successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted user successfully");
+});
+
 app.use("/", (req, res) => {
   res.send("Server Running Successfully");
 }); //this should always come at time as '/' is the prefix for all route
