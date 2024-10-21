e703b61a813715f6442ca35fcb463a972cf8cfdd
Tharanya <tharanyasugumaran@gamil.com>
2024-10-21 16:07:54 +0530

    database model and schema handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const UserSchema = require("./models/user");
 const { connectDB } = require("./config/database");
 
 const app = express();
+
+app.post("/login", async (req, res) => {
+  //creating a new instance for the user model
+  const user = new UserSchema({
+    firstName: "Harry",
+    lastName: "Potter",
+    emailId: "Harry.potter@gmail.com",
+    password: "Harry@1234",
+    age: 25,
+    gender: "Male",
+  });
+  try {
+    // throw new Error();
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    console.log("User could not be updated");
+    res.status(401).send("User failed to update");
+  }
+});
 
 connectDB()
   .then(() => {
