a96367da290128f99bfabd3847361093965628fe
Tharanya <tharanyasugumaran@gamil.com>
2024-10-22 19:18:24 +0530

    CRUD operations using mongoose model api

--- src/app.js
+++ src/app.js
@@ -4,16 +4,12 @@ const { connectDB } = require("./config/database");
 
 const app = express();
 
-app.post("/login", async (req, res) => {
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
   //creating a new instance for the user model
-  const user = new UserSchema({
-    firstName: "Harry",
-    lastName: "Potter",
-    emailId: "Harry.potter@gmail.com",
-    password: "Harry@1234",
-    age: 25,
-    gender: "Male",
-  });
+  // console.log(req.body);
+  const user = new UserSchema(req.body);
   try {
     // throw new Error();
     await user.save();
@@ -21,6 +17,69 @@ app.post("/login", async (req, res) => {
   } catch (err) {
     console.log("User could not be updated");
     res.status(401).send("User failed to update");
+  }
+});
+
+app.get("/getUser", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  try {
+    const user = await UserSchema.find({ emailId: userEmailId });
+    res.send(user);
+  } catch (err) {
+    res.status(401).send("Error Occurred");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await UserSchema.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(401).send("Error occured !!! ");
+  }
+});
+
+app.get("/getUserById", async (req, res) => {
+  const id = req.body._id;
+  try {
+    const user = await UserSchema.findById(id).exec();
+    // console.log(user);
+    if (user.length === 0) {
+      res.send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(401).send("Error Occurred");
+  }
+});
+
+//API to delete user
+app.delete("/user", async (req, res) => {
+  const userName = req.body; //deleting the user based on name
+  console.log("userName", userName);
+  try {
+    const user = await UserSchema.deleteOne(userName);
+    console.log(user.deletedCount);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(401).send("Error Occurred");
+    console.log(err);
+  }
+});
+
+//API - find and update by Id
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await UserSchema.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    res.send("User Updated successfully");
+    console.log(user);
+  } catch (err) {
+    res.status(401).send("Error Occurred");
   }
 });
 
